4a4ebfc1e7d91295a21f70c829ad41fc3c0c631c
John Adams <adamsjr8576@gmail.com>
2020-03-01 17:01:16 -0700

    Add Initial html wireframe with basic styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,54 @@
 <script>
-	export let name;
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header>
+		<h1>Harvard Art Museum</h1>
+	</header>
+	<h4>Please select a category below: </h4>
+	<section>
+		<article>
+			<h2>Period</h2>
+			<p>Contains the periods used to describe items
+ 			in the Harvard Art Museums collections.
+			</p>
+		</article>
+		<article>
+			<h2>Place</h2>
+			<p>Contains the location used to describe items
+ 			in the Harvard Art Museums collections.
+			</p>
+		</article>
+		<article>
+			<h2>Classification</h2>
+			<p>Contains the Classifications used to describe
+ 			item in the Harvard Art Museums collections.
+			</p>
+		</article>
+		<article>
+			<h2>Gallery</h2>
+			<p>Contains the Galleries and their items
+ 			in the Harvard Art Museums collections.
+			</p>
+		</article>
+	</section>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	header {
+		width: 100%;
+		height: 100px;
 	}
 
 	h1 {
@@ -20,11 +56,43 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+		margin: 0;
+		width: 100%;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	h4 {
+		width: 100%;
+		margin: 0;
+		margin-bottom: 25px;
 	}
+
+	section {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+		height: 550px;
+		width: 80%;
+	}
+
+	article {
+		width: 500px;
+		height: 275px;
+		border: 2px solid black;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+	}
+
+	h2 {
+		font-size: 2.5em;
+	}
+
+	p {
+		width: 80%;
+		text-align: center;
+	}
+
 </style>
