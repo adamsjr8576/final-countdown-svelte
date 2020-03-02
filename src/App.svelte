b7fd28ade62a86cd69e8a78512752ac4fbb9a5e3
John Adams <adamsjr8576@gmail.com>
2020-03-01 18:22:19 -0700

    Add background color, active, focus and hover states to article

--- src/App.svelte
+++ src/App.svelte
@@ -8,25 +8,25 @@
 	</header>
 	<h4>Please select a category below: </h4>
 	<section>
-		<article>
+		<article class='period-article'>
 			<h2>Period</h2>
 			<p>Contains the periods used to describe items
  			in the Harvard Art Museums collections.
 			</p>
 		</article>
-		<article>
+		<article class='place-article'>
 			<h2>Place</h2>
 			<p>Contains the location used to describe items
  			in the Harvard Art Museums collections.
 			</p>
 		</article>
-		<article>
+		<article class='classification-article'>
 			<h2>Classification</h2>
 			<p>Contains the Classifications used to describe
  			item in the Harvard Art Museums collections.
 			</p>
 		</article>
-		<article>
+		<article class='gallery-article'>
 			<h2>Gallery</h2>
 			<p>Contains the Galleries and their items
  			in the Harvard Art Museums collections.
@@ -78,12 +78,37 @@
 	article {
 		width: 500px;
 		height: 275px;
-		border: 2px solid black;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 		cursor: pointer;
+		tab-index: 10;
+	}
+
+	article:hover,
+	article:focus {
+		transform: scale(1.03);
+	}
+
+	article:active {
+		transform: scale(1);
+	}
+
+	.period-article {
+		background-color: #C4E790;
+	}
+
+	.place-article {
+		background-color: #90E7DE;
+	}
+
+	.classification-article {
+		background-color: #B390E7;
+	}
+
+	.gallery-article {
+		background-color: #E79098;
 	}
 
 	h2 {
