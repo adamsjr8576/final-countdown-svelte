ce2d3190fc912fcf7b3d007dfd1826fbe43181aa
John Adams <adamsjr8576@gmail.com>
2020-03-02 21:15:00 -0700

    Add loading mona lisa to isLoading component

--- src/IsLoading.svelte
+++ src/IsLoading.svelte
@@ -1,8 +1,10 @@
 <script>
+let src = './images/monalisa.png';
 </script>
 
 <section>
-  LOADING....
+  <img src={src} alt='image of mona losa' />
+  <p>Loading...</p>
 </section>
 
 <style>
@@ -11,6 +13,24 @@
     width: 100%;
     height: 100%;
     z-index: 100;
-    background-color: rgba(255, 255, 255, .5);
+    background-color: rgba(255, 255, 255, .7);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  img {
+    width: 200px;
+    animation: grow 5s linear infinite
+  }
+
+  @keyframes grow {
+    0% {
+      width: 200px;
+    }
+    100% {
+      width: 600px;
+    }
   }
 </style>
